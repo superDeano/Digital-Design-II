4061ecca972615c3f73c8dcb23e87945b21b9f2b
Dean <deancs1997@gmail.com>
2019-02-17 14:39:19 -0500

    Modified Code and PDF
    Modified the code a bit for readability

--- Labs/Lab 3/Code/testLab3.vhd
+++ Labs/Lab 3/Code/testLab3.vhd
@@ -4,56 +4,59 @@ use ieee.std_logic_unsigned.all;
 
 -- Entity of the converter
 entity converter is 
-port ( sign_mag : in std_logic_vector (3 downto 0);
-	twos_comp : out std_logic_vector (3 downto 0));
+port ( 	sign_mag : in std_logic_vector (3 downto 0);
+		twos_comp : out std_logic_vector (3 downto 0));
 end;
 
 -- The architecture of the converter
 architecture converter_arch of converter is 
 
+-- Constants to make the code more readable
 -- The number which is added at the second step of the 2's complement process
 constant numberToAddFor2sComplement : std_logic_vector(3 downto 0) := "0001";
 constant atMostSignificantIndex : integer := 3;
 
 begin
 
--- The process which will use the algorithm
-determining_number: process (sign_mag)
+	-- The process which will use the algorithm
+	determining_number: process (sign_mag)
 
--- 2 variables for easy manipulations
-variable temp : std_logic_vector (3 downto 0);
-variable temp_negated : std_logic_vector (3 downto 0);
+	-- 2 variables for easy manipulations
+	variable temp : std_logic_vector (3 downto 0);
+	variable temp_negated : std_logic_vector (3 downto 0);
 
-begin
+	begin
+	
+		-- Checks whether the number is positive
+		if ( sign(3) = 0) then 
+		twos_comp <= sign_mag;
 
--- Checks whether the number is positive
-if ( sign(3) = 0) then 
-twos_comp <= sign_mag;
+		-- Number is negative
+		else 
+		-- Assigns the variable the input to work with it
+		temp <= sign_mag;
+	
+		-- Goes in a loop to negate the number 
+		for at_index in 2 downto 0 loop
 
--- Number is negative
-else 
--- Assigns the variable the input to work with it
-temp <= sign_mag;
--- Goes in a loop to negate the number 
-for at_index in 2 downto 0 loop
+			if (temp(at_index) = 0) then
+			temp(at_index) <= 1;
+			else 
+			temp(at_index) <= 0;
+			end if;
+		
+		end loop;
 
-	if (temp(at_index) = 0) then
-	temp(at_index) <= 1;
-	else 
-	temp(at_index) <= 0;
-	end if;
-end loop;
+		temp_negated <= temp + numberToAddFor2sComplement;
 
-temp_negated <= temp + numberToAddFor2sComplement;
+		-- Making the number is still negative
+		temp_negated(atMostSignificantIndex) <= 1;
 
--- Making the number is still negative
-temp_negated(atMostSignificantIndex) <= 1;
+		-- The final output
+		twos_comp <= temp_negated;
 
--- The final output
-twos_comp <= temp_negated;
+		end if;
 
-end if;
-
-end process determining_number;
+	end process determining_number;
 
 end converter_arch;
